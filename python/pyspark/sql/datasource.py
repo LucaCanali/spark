7adb2ee08ff9629e0bcdf9fd88143d196a37767c
Luca Canali <luca.canali@cern.ch>
2024-07-02 15:28:30 +0200

    Add Arrow Batch support for the straming Python datasource too

--- python/pyspark/sql/datasource.py
+++ python/pyspark/sql/datasource.py
@@ -354,9 +354,10 @@ class DataSourceReader(ABC):
 
         Returns
         -------
-        iterator of tuples or :class:`Row`\\s
+        iterator of tuples or pyarrow RecordBatch
             An iterator of tuples or rows. Each tuple or row will be converted to a row
             in the final DataFrame.
+            It can also return an iterator of pyarrow RecordBatch if the data source supports it.
 
         Examples
         --------
@@ -451,8 +452,13 @@ class DataSourceStreamReader(ABC):
             message_parameters={"feature": "partitions"},
         )
 
+    if TYPE_CHECKING:
+        from pyarrow import RecordBatch
+    else:
+        RecordBatch = None
+
     @abstractmethod
-    def read(self, partition: InputPartition) -> Iterator[Tuple]:
+    def read(self, partition: InputPartition) -> Union[Iterator[Tuple], Iterator["RecordBatch"]]:
         """
         Generates data for a given partition and returns an iterator of tuples or rows.
 
@@ -474,9 +480,10 @@ class DataSourceStreamReader(ABC):
 
         Returns
         -------
-        iterator of tuples or :class:`Row`\\s
+        iterator of tuples or pyarrow RecordBatch
             An iterator of tuples or rows. Each tuple or row will be converted to a row
             in the final DataFrame.
+            It can also return an iterator of pyarrow RecordBatch if the data source supports it.
         """
         raise PySparkNotImplementedError(
             error_class="NOT_IMPLEMENTED",
